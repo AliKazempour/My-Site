fed70d2445ca65eef8987024f2f90c3c1804caa6
AliKazempour <Alikazempoor83@gmail.com>
2023-10-05 13:17:22 +0330

    📝 docs : updting views.py

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,47 @@
 from django.shortcuts import render
+from datetime import date
+
+posts = [
+    {
+        "slug": "the-chatbot",
+        "image": "chatgpt.png",
+        "author": "Ali Kazempour",
+        "date": date(2022, 7, 21),
+        "title": "ChatGPT",
+        "exerpt": "ChatGPT, which stands for Chat Generative Pre-trained Transformer,is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language",
+        "content": """
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        """
+    },
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Maximilian",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
 
 
 def starting_page(request):
@@ -9,5 +52,5 @@ def post(request):
     return render(request, "blog/all_posts.html")
 
 
-def post_detail(request,slug):
+def post_detail(request, slug):
     return render(request, "blog/post-detail.html")
