092c843a81dc999d15853a9d67a4ce7c63488b82
AliKazempour <Alikazempoor83@gmail.com>
2023-10-05 13:51:40 +0330

    📝 docs : updating the views.py

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "the-chatbot",
         "image": "chatgpt.png",
         "author": "Ali Kazempour",
         "date": date(2022, 7, 21),
         "title": "ChatGPT",
-        "exerpt": "ChatGPT, which stands for Chat Generative Pre-trained Transformer,is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language",
+        "excerpt": "ChatGPT, which stands for Chat Generative Pre-trained Transformer,is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language",
         "content": """
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
             sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
@@ -22,7 +22,7 @@ posts = [
     },
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
+        "image": "mountain.jpeg",
         "author": "Maximilian",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
@@ -44,8 +44,14 @@ posts = [
 ]
 
 
+def get_date(post):
+    return post['date']
+
+
 def starting_page(request):
-    return render(request, "blog/start_page.html")
+    sorted_posts = sorted(all_posts, key=get_date, reverse=True)
+    latest_posts = sorted_posts[:2]
+    return render(request, "blog/start_page.html", {"posts": latest_posts})
 
 
 def post(request):
