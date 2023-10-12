ff63053d860baccbee7bf4b4b14addfc7c47875d
AliKazempour <Alikazempoor83@gmail.com>
2023-10-12 22:30:03 +0330

    ✨ feat : add class PostAdmin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tag,Author,Post
+from .models import Tag, Author, Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date",)
+    list_filter = ("title", "author", "date", "tags")
+    search_fields = ("title", "author", "tags")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
