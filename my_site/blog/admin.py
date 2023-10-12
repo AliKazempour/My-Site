a85c5073899cad1459d32ec98919c2cb8176da33
AliKazempour <Alikazempoor83@gmail.com>
2023-10-12 21:51:05 +0330

    ✨ feat : registering Post,Author and Tag models

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag,Author,Post
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
