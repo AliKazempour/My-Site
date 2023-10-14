2475c393c8e252055da4790fc18e8d2b0a5335a3
AliKazempour <Alikazempoor83@gmail.com>
2023-10-14 10:21:29 +0330

    🎨 style : improve format of  starting_page view

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,47 +1,8 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Tag,Post,Author
 
-all_posts = [
-    {
-        "slug": "the-chatbot",
-        "image": "chatgpt.png",
-        "author": "Ali Kazempour",
-        "date": date(2022, 7, 21),
-        "title": "ChatGPT",
-        "excerpt": "ChatGPT, which stands for Chat Generative Pre-trained Transformer,is a large language model-based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language",
-        "content": """
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-        """
-    },
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountain.jpeg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
+all_posts = []
 
 
 def get_date(post):
@@ -49,9 +10,8 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date, reverse=True)
-    latest_posts = sorted_posts[:2]
-    return render(request, "blog/start_page.html", {"posts": latest_posts})
+    lastest_posts = Post.objects.all().order_by('-date')[:3]
+    return render(request, "blog/start_page.html", {"posts": lastest_posts})
 
 
 def post(request):
