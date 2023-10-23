dd55415720400798f69de51865a427f83304e8e7
AliKazempour <Alikazempoor83@gmail.com>
2023-10-23 23:20:30 +0330

    🎨 syle : improve structure of class PostDetailView(add post and get method)

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,9 +2,12 @@ from typing import Any
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Tag, Post, Author
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView, View
 from .models import Tag, Author, Post
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 class StartingPageView(ListView):
     model = Post
@@ -22,13 +25,26 @@ class PostsView(ListView):
     context_object_name = "all_posts"
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "blog/post-detail.html"
-    
-    def get_context_data(self, ** kwargs):
-        context = super().get_context_data(** kwargs)
-        context["post_tags"]= self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        return render(request, "blog/post-detail.html", context={
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        })
 
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        return render(request, "blog/post-detail.html", context={
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm
+        })
