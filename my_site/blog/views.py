86663112bdf7ad54a9e37b64bdd5a6382db1a312
AliKazempour <Alikazempoor83@gmail.com>
2023-10-23 18:07:02 +0330

    ✨ feat : add class PostDetailView

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,31 +1,20 @@
 from typing import Any
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from datetime import date
-from .models import Tag,Post,Author
-from django.views.generic import ListView
-from .models import Tag,Author,Post
-all_posts = []
+from .models import Tag, Post, Author
+from django.views.generic import ListView,DetailView
+from .models import Tag, Author, Post
 
-
-def get_date(post):
-    return post['date']
-
-
-# def starting_page(request):
-#     lastest_posts = Post.objects.all().order_by('-date')[:3]
-#     return render(request, "blog/start_page.html", {"posts": lastest_posts})
 
 class StartingPageView(ListView):
     model = Post
     template_name = "blog/start_page.html"
+
     def get_context_data(self, **kwargs):
-        context=super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context["posts"] = Post.objects.all().order_by('-date')[:3]
         return context
 
-# def post(request):
-#     all_posts = Post.objects.all().order_by("-date")
-#     return render(request, "blog/all_posts.html", {"all_posts": all_posts})
 
 class PostsView(ListView):
     model = Post
@@ -33,6 +22,12 @@ class PostsView(ListView):
     context_object_name = "all_posts"
 
 
-def post_detail(request, slug):
-    identified_post=get_object_or_404(Post,slug=slug)
-    return render(request, "blog/post-detail.html", {"post":identified_post,"post_tags":identified_post.tags.all()})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+    
+    def get_context_data(self, ** kwargs):
+        context = super().get_context_data(** kwargs)
+        context["post_tags"]= self.object.tags.all()
+        return context
+
